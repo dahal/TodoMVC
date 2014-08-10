87c27643c1598d139e1ef92347d98713d41a3d3e
Puru Dahal <pdahal@me.com>
2014-08-09 19:35:12 -0700

    Get js skeleton ready for addTodo

---
+++ app/assets/javascripts/todo/addTodo.js
@@ -0,0 +1,20 @@
+document.ready(function(){
+  var view = new View()
+  var add = new addTask()
+  this.controller = new Controller(view, add)
+})
+
+//===VIEW===//
+function View(){
+
+}
+
+//===CONTROLLER===//
+function Controller(){
+
+}
+
+//===MODEL===//
+function Model(){
+  
+}
