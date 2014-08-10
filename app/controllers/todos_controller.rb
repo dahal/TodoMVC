d50d050a10ee63cc2a8a01fb95e9f98d66737fcf
Puru Dahal <pdahal@me.com>
2014-08-09 19:13:49 -0700

    TDD controller create

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,16 @@
 class TodosController < ApplicationController
   def index
     @todos = Todo.all
+    @todo = Todo.new
+  end
+
+  def create
+    @todo = Todo.create(todo_params)
+    redirect_to '/'
+  end
+
+  private
+  def todo_params
+    params.require(:todo).permit(:task)
   end
 end
--- spec/controllers/todos_controller_spec.rb
+++ spec/controllers/todos_controller_spec.rb
@@ -2,11 +2,19 @@ require 'spec_helper'
 
 describe TodosController do
 
-  FactoryGirl.create(:todo)
-  context "#index" do
+  context "GET #index" do
+    FactoryGirl.create(:todo)
     it "lists the list of tasks" do
       get :index
       expect(assigns(:todos)).to eq Todo.all
+    end
+  end
+
+  context 'POST #create' do
+    it 'new task' do
+      post :create
+      let(:todo_params) {FactoryGirl.attributes_for(:todo)}
+      expect(Todo.create(todo_params)).to change(Todo.count).by(1)
     end
   end
 end
